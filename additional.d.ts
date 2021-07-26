fa8d5150e56d9396c2e06234e85aeaa495b12020
guzilla <gusrosales7@gmail.com>
2021-07-26 10:14:59 -0700

    Add missing fields to NextAPIRequest type

--- additional.d.ts
+++ additional.d.ts
@@ -1,5 +1,12 @@
+import { NextApiRequest } from 'next'
+import type { MongoClient, Db } from 'mongodb'
+
 declare namespace JSX {
   interface IntrinsicElements {
     jsx: boolean
   }
+}
+export interface CustomNextApiRequest extends NextApiRequest {
+  dbClient: MongoClient
+  db: Db
 }
